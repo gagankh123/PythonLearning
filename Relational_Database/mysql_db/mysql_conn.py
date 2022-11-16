b89ffcf40f53fababeb5eae3cf1880af936e9bf6
Gagan Khandelwal <gagan.khandelwal@o9solutions.com>
2022-11-17 01:14:37 +0530

    ADVANCED SQL QUERIES

--- Relational_Database/mysql_db/mysql_conn.py
+++ Relational_Database/mysql_db/mysql_conn.py
@@ -16,3 +16,14 @@ class mysql_connection:
             auth_plugin= strings.str_auth_plugin
         )
         self.mysql = self.conn.cursor(buffered=True)
+    
+    def _commit(self, close=True):
+        '''
+        
+        '''
+        self.conn.commit()
+        if close:
+            self._close(close=close)
+    
+    def _close(self, close=True):
+        self.conn.close()
